06face439281f03c891b898778bfc7f6fd79089f
kastastin <kastastin.main@gmail.com>
2025-01-18 21:30:02 +0200

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001...");
+});
+
+// TODO: 08:29
